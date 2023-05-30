0c4e1e52c2583af2fd1d54d1392ecd886f9de602
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-05-30 15:40:37 +0700

    написал второй обработчик, пока не тестировал

--- animal_food.py
+++ animal_food.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 animals = ['Кошка', 'Собака']
+weight = [1, 3, 5, 15, 25]
 
 
 class RequestFood(StatesGroup):
@@ -16,3 +17,17 @@ async def food_start(message: types.Message, state: FSMContext):
         keyboard.add(name)
     await message.answer("Выберите своего питомца:", reply_markup=keyboard)
     await state.set_state(RequestFood.waiting_for_pet_choice.state)
+
+
+async def food_chosen(message: types.Message, state: FSMContext):
+    if message.text.capitalize() not in animals:
+        await message.answer("Пожалуйста, выберите питомца, используя клавиатуру ниже.")
+        return
+    await state.update_data(chosen_food=message.text.capitalize())
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for size in weight:
+        keyboard.add(size)
+    await state.set_state(RequestFood.waiting_for_pet_weight.state)
+    await message.answer("Теперь выберите размер пачки:", reply_markup=keyboard)
+
